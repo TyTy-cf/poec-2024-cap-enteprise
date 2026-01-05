81619fe9b289a9d520e2d8a5230bcea20b990b8e
Kévin Tourret <kevin@drosalys.fr>
2026-01-05 11:05:12 +0100

    add gitignore

--- query.sql
+++
@@ -1,7 +0,0 @@
-
--- Affiche la note moyenne des jeux
-SELECT game.name, (SUM(review.rating)/COUNT(*))
-FROM review
-         JOIN game ON review.game_id = game.id
-GROUP BY game.id
-ORDER BY (SUM(review.rating)/COUNT(*)) DESC;
