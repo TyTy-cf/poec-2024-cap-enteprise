79f08132c7ee8510e655c2c96eab0e9885cff99d
Kévin Tourret <kevin@drosalys.fr>
2026-01-20 16:54:51 +0100

    update for allure

--- src/test/java/fr/kevin/cap_enterprise/gog/test/HomeTest.java
+++ src/test/java/fr/kevin/cap_enterprise/gog/test/HomeTest.java
@@ -3,7 +3,11 @@ package fr.kevin.cap_enterprise.gog.test;
 import fr.kevin.cap_enterprise.gog.BaseTest;
 import fr.kevin.cap_enterprise.gog.page.HomePage;
 import fr.kevin.cap_enterprise.gog.page.SearchResultsPage;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -15,6 +19,12 @@ public class HomeTest extends BaseTest {
                 .open()
                 .searchGame("Cyberpunk");
         assertTrue(searchResultsPage.getCurrentUrl().contains("Cyberpunk"));
+    }
+
+    @Step("Sauvegarder screenshot en cas d'erreur")
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] saveScreenshot() {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 
 }
